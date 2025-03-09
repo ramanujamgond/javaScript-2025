6948e07bd332eef641dfa95d55282ce25fefbb2e
Ramanujam Gond <ramanujamgond@gmail.com>
2025-03-09 18:06:24 +0530

    feat: dont the chess board problem

--- chess-board.js
+++ chess-board.js
@@ -1,11 +1,15 @@
 let size = 8;
-
+let board = "";
 
 for (let outerLoop = 0; outerLoop < size; outerLoop++) {
-    for (let innerloop = 0; innerloop <= size; innerloop++) {
-        if (size % 2 == 0) {
-            console.log("#");
-            // console.log("\n");
+    for (let innerloop = 0; innerloop < size; innerloop++) {
+        if ((outerLoop + innerloop) % 2 == 0) {
+            board += " ";
+        } else {
+            board += "#";
         }
     }
+    board += "\n";
 }
+
+console.log(board);
